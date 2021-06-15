52137fd732eb20c45478fca38db4c02875d9f367
Matt Kelly <bmatthewkelly@gmail.com>
2021-06-15 16:56:57 -0700

    set up sanity backend, add pizza and toppings schema

---
+++ sanity/schemas/pizza.js
@@ -0,0 +1,72 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
+export default {
+  name: 'pizza',
+  title: 'Pizzas',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the pizza in cents',
+      validation: (Rule) => Rule.min(1000).max(10000),
+      // TODO: add custom input component
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{ type: 'topping' }],
+        },
+      ],
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      // 2. return preview obj for the pizza
+      return {
+        title,
+        media,
+        subtitle: Object.values(tops).join(', '),
+      };
+    },
+  },
+};
