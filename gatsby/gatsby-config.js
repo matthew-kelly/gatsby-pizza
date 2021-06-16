12ff51aab63fac01c55f782ba16f15ca4f08feee
Matt Kelly <bmatthewkelly@gmail.com>
2021-06-16 11:22:16 -0700

    set up sanity dataset

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,25 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: `Gatsby Pizza`,
+    siteUrl: process.env.SANITY_SITE_URL,
+    description: `A sample site showcasing Gatsby with Sanity CMS`,
+  },
+  plugins: [
+    // can give just the name or an object with options
+    'gatsby-plugin-styled-components',
+    {
+      // name of the plugin
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
