9e17659bbe23832676f08e44a214e32e7019ed62
Matt Kelly <bmatthewkelly@gmail.com>
2021-06-16 11:48:16 -0700

    list pizzas on pizzas page

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,39 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  // NOTE: There is no loading state in gatsby! Don't have to wait for the data to return
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Hey! I'm the Pizzas page!</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+export const query = graphql`
+  query AllPizzasQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        id
+        name
+        price
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
